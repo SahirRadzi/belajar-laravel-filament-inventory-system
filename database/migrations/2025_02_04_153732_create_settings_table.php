3090a209b01ff2c87796f27dca04ce07bc55ea57
SahirRadzi <sahir.radzi@gmail.com>
2025-02-04 15:41:33 +0800

    add data in table settings

--- database/migrations/2025_02_04_153732_create_settings_table.php
+++ database/migrations/2025_02_04_153732_create_settings_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('key')->unique();
+            $table->text('value')->nullable();
+            $table->string('group')->nullable();
+            $table->json("attributes")->nullable();
+            $table->string('type')->default('text');
             $table->timestamps();
         });
     }
